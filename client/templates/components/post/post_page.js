26aabe17c3203ba4f573408790b7215175855968
cristopher29 <cristopher291194@gmail.com>
2016-05-10 17:37:23 +0200

    temporal fix

---
+++ client/templates/components/post/post_page.js
@@ -0,0 +1,19 @@
+/**
+ * Created by CristoH on 10/05/2016.
+ */
+
+Template.postPage.helpers({
+
+    youtubeId: function(){
+
+        if(this.youtubeUrl){
+            var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var match = this.youtubeUrl.match(regExp);
+            if (match && match[2].length == 11) {
+                return match[2];
+            }
+        }
+
+    }
+
+});
